e184271a08b6ce3e411d0961af8c1908dca367ba
Sugu Sougoumarane <ssougou@gmail.com>
2019-06-06 14:42:45 -0700

    vreplication: allow multi-value functions (#4914)
    The previous code (from old behavior) was still disallowing functions
    with more than one parameter. That check has been moved to 'sum'
    which is the only place where it should be enforced.
    
    Signed-off-by: Sugu Sougoumarane <ssougou@gmail.com>

--- go/vt/vttablet/tabletmanager/vreplication/table_plan_builder.go
+++ go/vt/vttablet/tabletmanager/vreplication/table_plan_builder.go
@@ -291,7 +291,7 @@ func (tpb *tablePlanBuilder) analyzeExpr(selExpr sqlparser.SelectExpr) (*colExpr
 		references: make(map[string]bool),
 	}
 	if expr, ok := aliased.Expr.(*sqlparser.FuncExpr); ok {
-		if expr.Distinct || len(expr.Exprs) != 1 {
+		if expr.Distinct {
 			return nil, fmt.Errorf("unexpected: %v", sqlparser.String(expr))
 		}
 		if aliased.As.IsEmpty() {
@@ -305,6 +305,9 @@ func (tpb *tablePlanBuilder) analyzeExpr(selExpr sqlparser.SelectExpr) (*colExpr
 			cexpr.operation = opCount
 			return cexpr, nil
 		case "sum":
+			if len(expr.Exprs) != 1 {
+				return nil, fmt.Errorf("unexpected: %v", sqlparser.String(expr))
+			}
 			aInner, ok := expr.Exprs[0].(*sqlparser.AliasedExpr)
 			if !ok {
 				return nil, fmt.Errorf("unexpected: %v", sqlparser.String(expr))
